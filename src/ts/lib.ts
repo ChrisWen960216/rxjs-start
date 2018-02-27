c7bec75f866872862d20b77dd8922837d8b0c0a9
ChrisWen960216 <chriswen96@163.com>
2018-02-27 13:33:38 +0800

    Add completed / cancel completed todoItem

--- src/ts/lib.ts
+++ src/ts/lib.ts
@@ -3,7 +3,7 @@ export const createTodoItem: (value: string | number) => Element | null = (value
   result.classList.add('list-group-item');
   const innerHTML: string = `${value}
   <button type="button" class="btn btn-default button-remove" aria-label="right Align">
-                              <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                             删除
                             </button>`;
   result.innerHTML = innerHTML;
   return result;
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -5,13 +5,31 @@ import { Observable } from 'rxjs';
 import { createTodoItem } from './lib';
 
 const $input: HTMLInputElement | null = <HTMLInputElement> document.querySelector('.input-text');
-const _input: any = Observable.fromEvent<KeyboardEvent>($input, 'keydown').filter((event: KeyboardEvent): boolean => event.keyCode === 13);
 const $list: HTMLUListElement | null = <HTMLUListElement> document.querySelector('#list');
+const $add: HTMLButtonElement | null = <HTMLButtonElement> document.querySelector('.btn-add');
+
+const $clickAdd: any = Observable.fromEvent<MouseEvent>($add, 'click');
+const $enter: any = Observable.fromEvent<KeyboardEvent>($input, 'keydown').filter((event: KeyboardEvent): boolean => event.keyCode === 13);
+const _input: any = $enter.merge($clickAdd);
 
 const $app: any = _input
   .map((): string|number => $input.value)
   .filter((value: string | number): boolean => { return value !== ''; })
   .map(createTodoItem)
-  .do((element: HTMLLIElement) => {$list.appendChild(element); })
+  .do((element: HTMLLIElement) => {
+    $list.appendChild(element);
+    $input.value = '';
+  })
+  .mergeMap(($todoItem: HTMLElement) => {
+    return Observable.fromEvent<MouseEvent>($todoItem, 'click')
+    .filter((event: MouseEvent): boolean => event.target === $todoItem).mapTo($todoItem);
+  })
+  .do(($todoItem: HTMLElement) => {
+    if ($todoItem.classList.contains('done')) {
+      $todoItem.classList.remove('done');
+    } else {
+      $todoItem.classList.add('done');
+    }
+  })
   .do((value: string|number): void => console.dir(value));
 $app.subscribe();
